262ba5cad804822b68a6d749c0b081ca75c07245
Thomas Lauer <thomaslauer01@gmail.com>
2018-04-29 15:54:01 -0700

    Updated it to a single sample

--- recordingTest.py
+++ recordingTest.py
@@ -1,20 +1,20 @@
 import numpy as nu
 import sounddevice as sd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-fs = 44100  # Set sampling frequency to 48000 hz
+fs = 44100  # Set sampling frequency to 44100 hz
 duration = 2
 
 
 def main():
 
     sample = sd.rec(fs*duration, samplerate=fs, channels=1, dtype='int16', blocking=1)
-    while 1:
-        sample = sd.playrec(sample, samplerate=fs, channels=1, dtype='int16', blocking=1)
-        print(len(sample))
-        #plt.figure(1)
-        #plt.title('Signal Wave...')
-        #plt.plot(sample)
-        #plt.show()
+    
+    sample = sd.playrec(sample, samplerate=fs, channels=1, dtype='int16', blocking=1)
+    print(len(sample))
+    plt.figure(1)
+    plt.title('Signal Wave...')
+    plt.plot(sample)
+    plt.show()
 
 main()

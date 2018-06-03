e2cbb0130e003c1a05881f96ffc21897a24fe6c1
Thomas Lauer <thomaslauer01@gmail.com>
2018-06-03 11:45:36 -0700

    Lots of comments, and removed extra testing files

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import sounddevice as sd
 import time
 import math
 
+"""
+This is the main program for the washing machine monitor
+"""
+
 # Import Adafruit library and create instance of REST client.
 from Adafruit_IO import Client
 aio = Client('853a9a70bd2c42508bfcb17a60105477')
@@ -14,6 +18,9 @@ duration = 10
 sleepTime = 0
 threshold = 250
 ARRAY_SIZE = 6
+
+# if more then 1/3 of the signals were positive, we're going to count it as
+# active
 ARRAY_THRESHOLD = .33
 
 def main():
@@ -24,17 +31,24 @@ def main():
     lastTFValue = -1
     while True:
         numIterations += 1
+
+        # Record a sound
         sample = sd.rec(int(fs*duration), samplerate=fs, channels=1, dtype='int16', blocking=1)
+        
+        # Remove the first 5% of the sound (This helped on some computers)
         sample = np.delete(sample,np.s_[0:int(len(sample)*0.05)])
 
+        # normalize the sample around an average of 0
         average = np.average(sample)
         sample = sample - average
-
+        
+        # Perform the FFT
         freqs, fft = performFFT(sample, fs)
+
+        # Calculate the average value, and normalize it for the sound duration
         integral = integrateFFT(freqs, fft, 250, 3000)/duration
 
-
-
+        # if it's above the threshold, set the ON value in the array
         if integral > threshold:
             a = [1] + a[:ARRAY_SIZE-1]
         else:
@@ -43,7 +57,9 @@ def main():
         aio.send('volume-level', integral) 
         
         if numIterations > ARRAY_SIZE:
+            # If we haven't had enough samples, don't report any on/off data
             if sum(a)/ARRAY_SIZE > ARRAY_THRESHOLD:
+                # It's currently active!
                 if lastTFValue != -1:
                     aio.send('on-slash-off', lastTFValue)
                 
@@ -51,6 +67,7 @@ def main():
                 aio.send('status-text', 'Wash On')
                 lastTFValue = 1
             else:   
+                # It's inactive right now
                 if lastTFValue != -1:
                     aio.send('on-slash-off', lastTFValue)
                 
@@ -63,11 +80,19 @@ def main():
 
 
 def performFFT(sample, fs):
+    """
+    This function performs an fft on the sample, given a sampling rate fs
+
+    It returns a list of frequencies, and a list of the amplitudes
+    """
     fft = fftpack.fft(sample)
     freqs = fftpack.fftfreq(len(fft)) * fs
     return freqs, np.abs(fft)
 
 def plotFFT(sample, fs):
+    """"
+    Plot an fft from a sample
+    """
     fig, ax = plt.subplots()
     ax.plot(sample)
     fig, ax = plt.subplots()
@@ -85,6 +110,9 @@ def plotFFT(sample, fs):
     plt.show()
 
 def integrateFFT(freqs, fft, low, high):
+    """
+    This samples an fft and averages all the frequencies between low and high
+    """
     sum = 0
     numSamples = 0
     for i in range(len(freqs)):
--- testConstantSend.py
+++
@@ -1,9 +0,0 @@
-# Import library and create instance of REST client.
-import time
-from Adafruit_IO import Client
-aio = Client('853a9a70bd2c42508bfcb17a60105477')
-
-def testConstantSend(status):
-	# Send the ON/OFF status to a visible graph feed.
-	aio.send('testConstantSend', status)
-	print(status)
